b24fbda0cc68ce491cb468261c36b5ce50112330
Joe Reger <joe@joereger.com>
2022-02-09 20:41:14 -0500

    trying another way

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5001;
 
 //If in dev, light up worker process.  In prod will use Heroku worker processes.
 //if (process.env.NODE_ENV !== 'production') {
-    const workers = require("./queue/strava-worker-starter");
+    require("./queue/strava-worker-starter").startStravaWorkers();
 //}
 
 
