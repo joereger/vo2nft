5cb28e4fdfb776c1dd2e7739ce3c1881c99f79db
Joe Reger <joe@joereger.com>
2022-02-21 20:37:55 -0500

    account background gradient to include some green

--- react-ui/src/components/AccountNavbar.jsx
+++ react-ui/src/components/AccountNavbar.jsx
@@ -97,7 +97,7 @@ const AccountNavbar = () => {
     </div>
   </header>
 
-  <div className="position-relative bg-gradient" style={{height: '480px'}}>
+  <div className="position-relative bg-gradient-vo2nft" style={{height: '480px'}}>
     <div className="shape shape-bottom shape-slant bg-secondary d-none d-lg-block">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3000 260">
         <polygon fill="currentColor" points="0,257 0,260 3000,260 3000,0"></polygon>
