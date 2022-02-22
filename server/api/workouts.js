077023c972a22539879e82ce2e49a24854093b58
Joe Reger <joe@joereger.com>
2022-02-21 20:54:07 -0500

    sort workouts

--- server/api/workouts.js
+++ server/api/workouts.js
@@ -5,16 +5,23 @@ const { Op } = require("sequelize");
 exports.workouts = async function(req, res){
     console.log('/api/workouts called');
 
-    const stravaAccount = await db.sequelize.models.StravaAccount.findOne({ where: {
-        userId: req.user.id   
-    } });
+    const stravaAccount = await db.sequelize.models.StravaAccount.findOne({ 
+        where: {
+            userId: req.user.id   
+        }
+    });
     console.log("req.user.id="+req.user.id);
 
     if (stravaAccount){
-        console.log("/api/workouts travaAccount.id=" + stravaAccount.id);
-        const workouts = await db.sequelize.models.Workout.findAll({ where: {
-            external_account_id: stravaAccount.id   
-        } });
+        console.log("/api/workouts stravaAccount.id=" + stravaAccount.id);
+        const workouts = await db.sequelize.models.Workout.findAll({ 
+            where: {
+                external_account_id: stravaAccount.id   
+            },
+            order: [
+                ['workout_date', 'DESC']
+            ]
+        });
         res.set('Content-Type', 'application/json');
         return res.send({workouts: workouts})
     } else {
